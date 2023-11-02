f574bddc2ce796dcd7b16a7c5101a28ff3d961dd
HARSHIT BHARDWAJ <harshitbhardwaj97@gmail.com>
2023-11-02 15:56:26 +0530

    Exercises 2.12-2.14 Added

--- Part2/phonebook/src/components/PhonebookForm.jsx
+++ Part2/phonebook/src/components/PhonebookForm.jsx
@@ -10,11 +10,22 @@ const PhonebookForm = ({
   return (
     <form onSubmit={submitHandler}>
       <div>
-        name: <input type="text" value={personName} onChange={onNameChange} />
+        name:{" "}
+        <input
+          type="text"
+          value={personName}
+          onChange={onNameChange}
+          required
+        />
       </div>
       <div>
         number:{" "}
-        <input type="text" value={personNumber} onChange={onNumberChange} />
+        <input
+          type="text"
+          value={personNumber}
+          onChange={onNumberChange}
+          required
+        />
       </div>
       <div>
         <button type="submit">Add</button>
