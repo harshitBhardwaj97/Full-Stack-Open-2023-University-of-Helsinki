1669345e770ae3ce22f118f064e81d76d1e72ba6
HARSHIT BHARDWAJ <harshitbhardwaj97@gmail.com>
2023-11-03 12:32:56 +0530

    Exercises 2.16-2.17 Solution added (Phonebook)

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -3,12 +3,16 @@ import phonebookService from "./services/phonebook-service";
 import Filter from "./components/Filter";
 import PhonebookForm from "./components/PhonebookForm";
 import PersonsList from "./components/PersonsList";
+import NotificationMessage from "./components/NotificationMessage";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setnewPhoneNumber] = useState("");
   const [search, setSearch] = useState("");
+  const [message, setMessage] = useState(null);
+  // const [status, setStatus] = useState(null);
+  // const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     phonebookService
@@ -19,6 +23,15 @@ const App = () => {
       })
       .catch((error) => console.error(error));
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message]);
 
   const handlePersonSubmit = (e) => {
     const newPerson = { name: newName, phonenumber: newPhoneNumber };
@@ -41,43 +54,34 @@ const App = () => {
           })
           .then((response) => {
             console.log(response, response.data);
+            setMessage(`${newName} information added in phonebook !`);
+          })
+          .catch((error) => {
+            console.error(error);
+            setMessage(
+              `Information of ${newName} has already been removed from phonebook !`
+            );
           });
-        /*
-        phonebookService.getAllNumbers().then((response) => {
-          const allPersons = response.data;
-          console.log(allPersons);
-          const foundPerson = allPersons.filter(
-            (person) => person.name === newName
-          );
-          console.log(foundPerson, foundPerson[0].id);
-          phonebookService
-            .updateExistingPerson(foundPerson[0].id, {
-              ...foundPerson[0],
-              phonenumber: newPhoneNumber,
-            })
-            .then((response) => {
-              console.log(response.data);
-            });
-        });*/
       }
       setNewName("");
       setnewPhoneNumber("");
     } else {
       phonebookService.addPerson(newPerson).then((response) => {
-        console.log(response, response.data);
+        // console.log(response, response.data);
         setPersons(persons.concat(newPerson));
-        setNewName("");
-        setnewPhoneNumber("");
+        setMessage(`${newName} added in the phonebook !`);
+        // setNewName("");
+        // setnewPhoneNumber("");
       });
     }
   };
-
   const handleDelete = (name, id) => {
     if (window.confirm(`Are you sure you want to delete ${name} ?`)) {
       phonebookService.deletePerson(id).then((response) => {
         const updatedPersons = persons.filter((person) => person.id !== id);
         console.log(response.data);
         setPersons(updatedPersons);
+        setMessage(`${newName} deleted from phonebook !`);
       });
     }
   };
@@ -93,6 +97,8 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+      {/* {message !== null && <NotificationMessage message={message} />} */}
+      <NotificationMessage message={message} />
       <Filter value={search} onSmash={handleSearch} />
       <h2>Add a New</h2>
       <PhonebookForm
